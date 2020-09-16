3f48dbcdb368b2b9a42760f41707680f3ba74147
Joni <pacificjoni@gmail.com>
2020-09-16 15:27:51 +0600

    Updates

---
+++ precache-manifest.d46547f47e538a022dda80ec5f9c4360.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f2e52adc3b41e285d888c91e3f1b3a",
+    "url": "/klinarmen_react/index.html"
+  },
+  {
+    "revision": "1526231ba11cda520326",
+    "url": "/klinarmen_react/static/css/2.12d18803.chunk.css"
+  },
+  {
+    "revision": "d5494323aa2ae01c4fe9",
+    "url": "/klinarmen_react/static/css/main.c3a36631.chunk.css"
+  },
+  {
+    "revision": "1526231ba11cda520326",
+    "url": "/klinarmen_react/static/js/2.d117f4ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/klinarmen_react/static/js/2.d117f4ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5494323aa2ae01c4fe9",
+    "url": "/klinarmen_react/static/js/main.dfd29cae.chunk.js"
+  },
+  {
+    "revision": "71cb7587a6e2b582b28c",
+    "url": "/klinarmen_react/static/js/runtime-main.ba463aed.js"
+  },
+  {
+    "revision": "829d452c64c4b55779e2140db2c59edb",
+    "url": "/klinarmen_react/static/media/404.829d452c.png"
+  },
+  {
+    "revision": "907b74de12b89805612e69cd9ece33f6",
+    "url": "/klinarmen_react/static/media/ElegantIcons.907b74de.svg"
+  },
+  {
+    "revision": "d72ad3f702b9f23540e8ed78b4b65749",
+    "url": "/klinarmen_react/static/media/ElegantIcons.d72ad3f7.eot"
+  },
+  {
+    "revision": "f9d179f59b0878ffcd32a5b3c8ae9c62",
+    "url": "/klinarmen_react/static/media/ElegantIcons.f9d179f5.ttf"
+  },
+  {
+    "revision": "fdd9e757bf61675343dcf55100422b84",
+    "url": "/klinarmen_react/static/media/ElegantIcons.fdd9e757.woff"
+  },
+  {
+    "revision": "7fca61530a20f2ad9322b143f38dc5c2",
+    "url": "/klinarmen_react/static/media/about.7fca6153.png"
+  },
+  {
+    "revision": "b3252a0499a84379472f65bdea177a12",
+    "url": "/klinarmen_react/static/media/about_2.b3252a04.jpg"
+  },
+  {
+    "revision": "8b9911b09ad887309b2a30600928b6d3",
+    "url": "/klinarmen_react/static/media/blog_1.8b9911b0.jpg"
+  },
+  {
+    "revision": "a82e42dcc71bb2c346f83517795abe21",
+    "url": "/klinarmen_react/static/media/blog_2.a82e42dc.jpg"
+  },
+  {
+    "revision": "7177c6f66926e905a8f6db7ebe4cd822",
+    "url": "/klinarmen_react/static/media/blog_3.7177c6f6.jpg"
+  },
+  {
+    "revision": "257350cc0a20fae617e9da5303996d26",
+    "url": "/klinarmen_react/static/media/custom-icons.257350cc.woff"
+  },
+  {
+    "revision": "3b3dc087de87737e152c3fa6e00455c1",
+    "url": "/klinarmen_react/static/media/custom-icons.3b3dc087.eot"
+  },
+  {
+    "revision": "46ae079e134287ba12f60eda43373c02",
+    "url": "/klinarmen_react/static/media/custom-icons.46ae079e.ttf"
+  },
+  {
+    "revision": "e805f76b5a10bd60304722275d5f2847",
+    "url": "/klinarmen_react/static/media/custom-icons.e805f76b.svg"
+  },
+  {
+    "revision": "8fe6c57817155b1477783d72f7891762",
+    "url": "/klinarmen_react/static/media/fact.8fe6c578.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/klinarmen_react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/klinarmen_react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/klinarmen_react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/klinarmen_react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/klinarmen_react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "eb0bf40c4605b9d58ef7aaf4bb040b82",
+    "url": "/klinarmen_react/static/media/page_bg.eb0bf40c.jpg"
+  },
+  {
+    "revision": "6dc0808de061bce5ecd01ef5b3debcc3",
+    "url": "/klinarmen_react/static/media/pre_loader.6dc0808d.gif"
+  },
+  {
+    "revision": "8eddaffaab12fc4c7e48ff3136681969",
+    "url": "/klinarmen_react/static/media/service1.8eddaffa.jpg"
+  },
+  {
+    "revision": "cadf45d25561e1864fbc68dba62bcf10",
+    "url": "/klinarmen_react/static/media/service2.cadf45d2.jpg"
+  },
+  {
+    "revision": "7fc08f40bb225f21b40f9e9744ffe3a2",
+    "url": "/klinarmen_react/static/media/service3.7fc08f40.jpg"
+  },
+  {
+    "revision": "2dcf6bb047ea3742c476f7551f4efd22",
+    "url": "/klinarmen_react/static/media/service4.2dcf6bb0.jpg"
+  },
+  {
+    "revision": "10f7cea6f64de08cd89fe62c89099015",
+    "url": "/klinarmen_react/static/media/service_single.10f7cea6.jpg"
+  },
+  {
+    "revision": "386854874ffdde3b3a69edd78abf2f6c",
+    "url": "/klinarmen_react/static/media/slider1.38685487.jpg"
+  },
+  {
+    "revision": "43d682420a71a1cacc7869d002b1b510",
+    "url": "/klinarmen_react/static/media/slider11.43d68242.jpg"
+  },
+  {
+    "revision": "5eec6a43049d48918ad40a6ed525561c",
+    "url": "/klinarmen_react/static/media/slider2.5eec6a43.jpg"
+  },
+  {
+    "revision": "740e50f738188cb67ba0828aaa6c6686",
+    "url": "/klinarmen_react/static/media/slider22.740e50f7.jpg"
+  },
+  {
+    "revision": "84cdbcc295684c42b7701d53534e6db0",
+    "url": "/klinarmen_react/static/media/slider3.84cdbcc2.jpg"
+  },
+  {
+    "revision": "9ae4fa217f5a4e06dd913b063708db4d",
+    "url": "/klinarmen_react/static/media/team-1.9ae4fa21.jpg"
+  },
+  {
+    "revision": "005bbade709a919cf61c58064c1a67f9",
+    "url": "/klinarmen_react/static/media/team-2.005bbade.jpg"
+  },
+  {
+    "revision": "dbf5c8313e85d65a42328ca25e22dc02",
+    "url": "/klinarmen_react/static/media/team-3.dbf5c831.jpg"
+  },
+  {
+    "revision": "63aa24bcc632f1b2982016f196731a52",
+    "url": "/klinarmen_react/static/media/team-4.63aa24bc.jpg"
+  },
+  {
+    "revision": "238d31e92144963481d67ac23131f875",
+    "url": "/klinarmen_react/static/media/team_bg.238d31e9.jpg"
+  },
+  {
+    "revision": "d22979885f7f460a82a8ceb50e4d4a25",
+    "url": "/klinarmen_react/static/media/testimonial.d2297988.jpg"
+  },
+  {
+    "revision": "ffcfb867be0fb42b81c9118e12530b14",
+    "url": "/klinarmen_react/static/media/work_1.ffcfb867.jpg"
+  },
+  {
+    "revision": "848ae555b73b3f9a79187baa27edac77",
+    "url": "/klinarmen_react/static/media/work_2.848ae555.jpg"
+  },
+  {
+    "revision": "1933dfc621da978161d31b674998ee73",
+    "url": "/klinarmen_react/static/media/work_3.1933dfc6.jpg"
+  },
+  {
+    "revision": "b13fae3c18536173874a58e5b35a15f3",
+    "url": "/klinarmen_react/static/media/work_4.b13fae3c.jpg"
+  },
+  {
+    "revision": "55c249b5c4866c76584be86b918395de",
+    "url": "/klinarmen_react/static/media/work_5.55c249b5.jpg"
+  },
+  {
+    "revision": "546fe98d4574838fd0d202edfcbc95c4",
+    "url": "/klinarmen_react/static/media/work_6.546fe98d.jpg"
+  },
+  {
+    "revision": "1e3105a0747de1f336af738f9b37bd3f",
+    "url": "/klinarmen_react/static/media/work_7.1e3105a0.jpg"
+  },
+  {
+    "revision": "e8c92dd880798e348b94e5fd5e840098",
+    "url": "/klinarmen_react/static/media/work_8.e8c92dd8.jpg"
+  },
+  {
+    "revision": "71f4a08d106a84e05a42e4a248898c11",
+    "url": "/klinarmen_react/static/media/work_bg.71f4a08d.jpg"
+  },
+  {
+    "revision": "ec426257531abbaa423f4b4e809c8c86",
+    "url": "/klinarmen_react/static/media/work_single_1.ec426257.jpg"
+  },
+  {
+    "revision": "63e32054d3574a650f71346d6e03081e",
+    "url": "/klinarmen_react/static/media/work_single_2.63e32054.jpg"
+  }
+]);
